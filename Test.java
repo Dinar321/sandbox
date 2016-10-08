7a3e38c48a20754822cac46e0f83d3eda3e0b5e6
Ilya Labun <ilya92labun@gmail.com>
2016-10-08 16:52:00 +0300

    First commit

---
+++ Test.java
@@ -0,0 +1,12 @@
+
+
+public class Test {
+	
+	public static void main(String[] args) {
+		int a = 33;
+		int b = 32;
+		int хуй = (a + b);
+		double c = (хуй * хуй)/2.0;
+		System.out.println(" c = " + c); 
+	}
+} 
